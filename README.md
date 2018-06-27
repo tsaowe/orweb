58e2b989bc9bf512f6b093772a3c1d24bd789e97
max.cao <wei.cao@orcadt.com>
2018-06-27 11:31:58 +0800

    master - for web and mobile

---
+++ README.md
@@ -0,0 +1,3 @@
+### common cnd info
+
+<script src="http://apps.bdimg.com/libs/jquery/2.1.4/jquery.min.js"></script>
--- index.js
+++ index.js
@@ -2,21 +2,16 @@ const Koa = require('koa');
 const app = new Koa();
 const Router = require('koa-router');
 const route = new Router();
-
-const render = require('koa-ejs');
 const path = require('path');
-
-render(app, {
-    root: path.join(__dirname, 'view'),
-    layout: 'template',
-    viewExt: 'html',
-    cache: true,
-    debug: false
-});
+const viewFolder = path.join(__dirname, 'view');
 
 route.all('/', async ctx => {
-    await ctx.render('page-list/user', {name:'caowei'});
+    await ctx.render('page-list/user', {name: 'caowei'});
 });
 
+
 app.use(route.routes()).use(route.allowedMethods());
-app.listen(3000);
+
+const port = 3000;
+app.listen(port);
+console.log(`open http://localhost:${port}/`);
